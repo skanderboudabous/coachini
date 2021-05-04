88557cbd321c8141743929b034b6066fede113e8
Mseddi Mohamed <mseddi.mohamed@iit.ens.tn>
2021-05-04 09:36:00 +0100

    signin and primary button

--- lib/pages/sigin.dart
+++ lib/pages/sigin.dart
@@ -2,13 +2,14 @@ import 'package:coachini/constants/app_routes.dart';
 import 'package:coachini/controller/firebase_controller.dart';
 import 'package:coachini/helpers/helpers.dart';
 import 'package:coachini/widgets/label_button.dart';
-import 'package:primary_button/PrimaryButton.dart';
+import 'package:coachini/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'dart:core';
 import 'package:get/get.dart';
 import 'package:loader_overlay/loader_overlay.dart';
+
 class SignInPage extends StatefulWidget {
   @override
   _SignInPageState createState() => _SignInPageState();
@@ -16,111 +17,131 @@ class SignInPage extends StatefulWidget {
 
 class _SignInPageState extends State<SignInPage> {
   final FirebaseController authController = FirebaseController.to;
-  final  _formKey = GlobalKey<FormBuilderState>();
+  final _formKey = GlobalKey<FormBuilderState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  late bool hide=true;
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: LoaderOverlay(
         useDefaultLoading: true,
-        child: Stack(
-          children:[
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/bg.jpg"),
-                  fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg.jpg"),
+                fit: BoxFit.cover,
+                colorFilter:  ColorFilter.mode(
+                  Colors.grey,
+                  BlendMode.darken,
                 ),
               ),
             ),
-            Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/logo.png"),
-                      colorFilter: new ColorFilter.mode(Colors.blue.withOpacity(1.0), BlendMode.darken),
-                      fit: BoxFit.cover
-                    ),
-                  ),
-                ),
-                FormBuilder(
-                  key: _formKey,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40.0),
-                    child: Center(
-                      child: SingleChildScrollView(
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Image.asset('assets/images/logo.png',
-                              height: 100,
-                                width: 150,
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.7),
+                        borderRadius: BorderRadiusDirectional.circular(28)),
+                    child: FormBuilder(
+                      key: _formKey,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                        child: Center(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  width: 75,
+                                  height: 75,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage("assets/images/logo.png"),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                                FormBuilderTextField(
+                                  controller: emailController,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(Icons.email),
+                                      labelText: 'Email'),
+                                  validator: Validator().email,
+                                  keyboardType: TextInputType.emailAddress,
+                                  onChanged: (value) => null,
+                                  onSaved: (value) =>
+                                      emailController.text = value!,
+                                  name: 'email',
+                                ),
 
-                              ),
-                              FormBuilderTextField(
-                                controller: emailController,
-                                decoration: InputDecoration(
-                                    prefixIcon: Icon(Icons.email), labelText: 'Email'),
-                                validator: Validator().email,
-                                keyboardType: TextInputType.emailAddress,
-                                onChanged: (value) => null,
-                                onSaved: (value) => emailController.text = value!,
-                                name: 'email',
-                              ),
+                                FormBuilderTextField(
+                                  controller: passwordController,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(Icons.lock),
+                                      suffixIcon: IconButton(
+                                        icon: Icon(
+                                          Icons.remove_red_eye,
+                                          color: this.hide ? Colors.blue : Colors.grey,
+                                        ),
+                                        onPressed: () {
+                                          setState(() => this.hide = !this.hide);
+                                        },
+                                      ),
+                                      labelText: 'Password'),
+                                  validator: Validator().password,
+                                  obscureText: hide,
+                                  onChanged: (value) => null,
+                                  onSaved: (value) =>
+                                      passwordController.text = value!,
+                                  maxLines: 1,
+                                  name: 'password',
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                PrimaryButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      authController.login(
+                                          email: emailController.text,
+                                          password: passwordController.text,
+                                          context: context);
+                                    }
+                                  },
+                                  buttonLabel: 'Sign In',
+                                  fontSize: 14,
+                                  bgColor: Colors.lightBlue,
+                                  width: 150,
+                                  height: 50,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                PrimaryButton(
+                                  onPressed: () =>
+                                      Get.toNamed(AppRoutes.SIGNUP),
+                                  buttonLabel: 'Signup',
+                                  fontSize: 14,
+                                  bgColor: Colors.lightBlue,
+                                  width: 150,
+                                  height: 50,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
 
-                              FormBuilderTextField(
-                                controller: passwordController,
-                                decoration: InputDecoration(
-                                    prefixIcon: Icon(Icons.lock), labelText: 'Password'),
-                                validator: Validator().password,
-                                obscureText: true,
-                                onChanged: (value) => null,
-                                onSaved: (value) => passwordController.text = value!,
-                                maxLines: 1,
-                                name: 'password',
-                              ),
-                              SizedBox(
-                                height: 10 ,
-                              ),
-                              PrimaryButton(
-                                onPressed:() async {
-                                      if (_formKey.currentState!.validate()) {
-                                        authController.login(email: emailController.text,password: passwordController.text,context:context);
-                                      }},
-                                buttonLabel: 'Sign In',
-                                fontSize: 14,
-                                bgColor:Colors.lightBlue,
-                                width:340,
-                                height:50,
-                              ),
-                              SizedBox(
-                                height: 10 ,
-                              ),
-                              PrimaryButton(
-                                onPressed: () => Get.toNamed(AppRoutes.SIGNUP),
-                                buttonLabel: 'Signup',
-                                fontSize: 14,
-                                bgColor:Colors.lightBlue,
-                                width:340,
-                                height:50,
-                              ),
-                              
 //                              LabelButton(
 //                                labelText: 'Password Reset',
 //                                onPressed: () => print("Password Reset"),
 //                              ),
-
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -129,10 +150,9 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ],
             ),
-          ]
+          ),
         ),
       ),
     );
   }
 }
-
--- lib/widgets/primary_button.dart
+++ lib/widgets/primary_button.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
-class PrimaryButton extends StatelessWidget {
-  PrimaryButton({required this.labelText, required this.onPressed});
 
-  final String labelText;
-  final void Function() onPressed;
+class PrimaryButton extends StatelessWidget {
+  /// This is the button label text
+  final String? buttonLabel;
+
+  /// on press function
+  final VoidCallback? onPressed;
+
+  /// This is the font size of the text
+  final double? fontSize;
+
+  /// This is the color of the button's text
+  final Color? textColor;
+
+  /// This is the color of the button's text
+  final Color? bgColor;
+
+  /// This is width of button
+  final double? width;
+
+  /// This is height of button
+  final double? height;
+
+  const PrimaryButton(
+      {this.buttonLabel,
+        this.bgColor,
+        this.width,
+        this.height,
+        this.fontSize,
+        this.textColor,
+        required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
-      style: ButtonStyle(),
-      child: Text(
-        labelText.toUpperCase(),
-        style: TextStyle(fontWeight: FontWeight.bold),
-      ),
-    );
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          primary: this.bgColor,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(28))),),
+        child: Container(
+            height: height,
+            width: width,
+            child: Center(
+              child: Text(
+                buttonLabel??'',
+                style: TextStyle(
+                    fontSize: fontSize,
+                    color: textColor,
+                    fontWeight: FontWeight.bold),
+              ),
+            )));
   }
 }
